743a1cfa51bd3845280e1c90a992394051a5b3b4
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-23 23:46:25 +0900

    Apply FlightSearchApp

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/MainActivity.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.flightsearchapp.ui.FlightSearchApp
 import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +22,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    FlightSearchApp(modifier = Modifier.fillMaxSize())
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun FlightSearchAppPreview() {
     FlightSearchAppTheme {
-        Greeting("Android")
+        FlightSearchApp(modifier = Modifier.fillMaxSize())
     }
 }
