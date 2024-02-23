2c5ae9df494892a8011d4ff3298f97d2b3015435
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-23 23:26:07 +0900

    Update gradle - hilt-android:2.50 - hilt-android-compiler:2.50

--- Compose-Flight-Search-App/app/build.gradle.kts
+++ Compose-Flight-Search-App/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -50,6 +51,9 @@ android {
 }
 
 dependencies {
+
+    implementation("com.google.dagger:hilt-android:2.50")
+    ksp("com.google.dagger:hilt-android-compiler:2.50")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
