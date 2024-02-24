e7b75f7dc093aa67bc9f62a0d9fcb6e29b78c737
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-24 13:41:34 +0900

    Apply AndroidEntryPoint.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/MainActivity.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.flightsearchapp.ui.FlightSearchApp
 import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
