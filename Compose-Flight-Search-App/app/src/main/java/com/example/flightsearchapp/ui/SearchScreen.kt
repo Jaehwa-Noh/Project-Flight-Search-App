d90986d4eba9490d3f011fbabd495608d1129162
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-24 12:45:24 +0900

    Apply state at searchbar - searchQuery, onSearch, searchQuery.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/SearchScreen.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/SearchScreen.kt
@@ -9,23 +9,49 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SearchBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun SearchScreen(modifier: Modifier = Modifier) {
+    var searchQuery: String by remember {
+        mutableStateOf("")
+    }
+
+    val onQueryChange: (String) -> Unit = {
+        searchQuery = it
+    }
+
+    val onSearch: (String) -> Unit = {
+
+    }
+
     Column(modifier = modifier) {
-        FlightSearchBar(modifier = Modifier.fillMaxWidth())
+        FlightSearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            query = searchQuery,
+            onQueryChange = onQueryChange,
+            onSearch = onSearch,
+        )
         SearchFlightListScreen(modifier = Modifier.fillMaxSize())
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FlightSearchBar(modifier: Modifier = Modifier) {
+fun FlightSearchBar(
+    modifier: Modifier = Modifier,
+    query: String,
+    onQueryChange: (String) -> Unit,
+    onSearch: (String) -> Unit,
+) {
     SearchBar(
-        query = "",
-        onQueryChange = {},
-        onSearch = {},
+        query = query,
+        onQueryChange = onQueryChange,
+        onSearch = onSearch,
         active = false,
         onActiveChange = {},
         leadingIcon = {
