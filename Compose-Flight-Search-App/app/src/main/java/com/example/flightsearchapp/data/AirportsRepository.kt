609a54e7a84f3b99269bfdaaa468bc4284765b4b
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-24 20:02:06 +0900

    Transform stream to SearchedAirportsStream.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/AirportsRepository.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/AirportsRepository.kt
@@ -1,20 +1,29 @@
 package com.example.flightsearchapp.data
 
-import com.example.flightsearchapp.data.database.Airport
+import com.example.flightsearchapp.ui.model.SearchedAirport
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface AirportsRepository {
-    suspend fun getSuggestionsStream(query: String): Flow<List<Airport>>
+    suspend fun getSuggestionsStream(query: String): Flow<List<SearchedAirport>>
 }
 
 @Singleton
 class InDiskAirportsRepository @Inject constructor(
     private val airportsDataSource: AirportsDataSource,
 ) : AirportsRepository {
-    override suspend fun getSuggestionsStream(query: String): Flow<List<Airport>> {
-        return airportsDataSource.getSuggestionsStream(query = query)
+    override suspend fun getSuggestionsStream(query: String): Flow<List<SearchedAirport>> {
+        return airportsDataSource.getSuggestionsStream(query = query).map {
+            it.map { airport ->
+                SearchedAirport(
+                    id = airport.id,
+                    iataCode = airport.iataCode,
+                    name = airport.name,
+                )
+            }
+        }
     }
 }
 
