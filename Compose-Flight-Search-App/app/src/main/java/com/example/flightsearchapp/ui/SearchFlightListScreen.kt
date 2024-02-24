13f52b8416855aa25cd10edf92530c3ceb5aa16e
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-24 11:31:34 +0900

    Create SearchFlightListScreen.

---
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/SearchFlightListScreen.kt
@@ -0,0 +1,10 @@
+package com.example.flightsearchapp.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchFlightListScreen(modifier: Modifier = Modifier) {
+    Text("List", modifier = modifier)
+}
