a5484308275d801c2e21e84677179082b04f9b52
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-25 20:56:09 +0900

    Create AirportItemCard.

---
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/AirportItemCard.kt
@@ -0,0 +1,12 @@
+package com.example.flightsearchapp.ui
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AirportItemCard(modifier: Modifier = Modifier) {
+    Card(onClick = {  }) {
+        
+    }
+}
