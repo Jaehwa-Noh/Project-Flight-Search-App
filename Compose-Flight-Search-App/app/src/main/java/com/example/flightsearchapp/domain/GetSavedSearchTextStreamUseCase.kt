0ca4429f94e6adea25bbdb5d689bab89f2448f88
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-27 17:58:40 +0900

    Remove suspend keyword.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/domain/GetSavedSearchTextStreamUseCase.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/domain/GetSavedSearchTextStreamUseCase.kt
@@ -1,19 +1,12 @@
 package com.example.flightsearchapp.domain
 
 import com.example.flightsearchapp.data.SearchTextRepository
-import com.example.flightsearchapp.di.DispatcherDefault
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetSavedSearchTextStreamUseCase @Inject constructor(
     private val searchTextRepository: SearchTextRepository,
-    @DispatcherDefault private val defaultDispatcher: CoroutineDispatcher
 ) {
-    suspend operator fun invoke(): Flow<String> {
-        return withContext(defaultDispatcher) {
-            searchTextRepository.savedSearchText
-        }
-    }
+    operator fun invoke(): Flow<String> =
+        searchTextRepository.savedSearchText
 }
