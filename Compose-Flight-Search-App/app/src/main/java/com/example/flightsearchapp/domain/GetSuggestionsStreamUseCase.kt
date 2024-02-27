fc2832286a528405e678cc4db85a4da6e3905850
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-27 16:03:50 +0900

    Create GetSuggestionsStreamUseCase.

---
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/domain/GetSuggestionsStreamUseCase.kt
@@ -0,0 +1,20 @@
+package com.example.flightsearchapp.domain
+
+import com.example.flightsearchapp.data.AirportsRepository
+import com.example.flightsearchapp.di.DispatcherDefault
+import com.example.flightsearchapp.ui.model.SearchedAirport
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetSuggestionsStreamUseCase @Inject constructor(
+    private val airportsRepository: AirportsRepository,
+    @DispatcherDefault private val defaultDispatcher: CoroutineDispatcher
+) {
+    suspend operator fun invoke(query: String): Flow<List<SearchedAirport>> {
+        return withContext(defaultDispatcher) {
+            airportsRepository.getSuggestionsStream(query = query)
+        }
+    }
+}
