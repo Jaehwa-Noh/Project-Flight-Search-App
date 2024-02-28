d6cb27383886d24f303cffebc866d681a493cb8a
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-28 17:04:55 +0900

    Create SuggestionsListItem.

---
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/SuggestionsListItem.kt
@@ -0,0 +1,49 @@
+package com.example.flightsearchapp.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.flightsearchapp.ui.model.SearchedAirport
+import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
+
+@Composable
+fun SuggestionsListItem(
+    modifier: Modifier = Modifier,
+    searchedAirport: SearchedAirport,
+) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier,
+    ) {
+        Text(
+            text = searchedAirport.iataCode,
+            style = MaterialTheme.typography.titleSmall
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = searchedAirport.name)
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SuggestListItemCardPreview() {
+    FlightSearchAppTheme(dynamicColor = false) {
+        SuggestionsListItem(
+            searchedAirport = SearchedAirport(
+                id = 1,
+                iataCode = "ABC",
+                name = "Airport"
+            )
+        )
+    }
+}
