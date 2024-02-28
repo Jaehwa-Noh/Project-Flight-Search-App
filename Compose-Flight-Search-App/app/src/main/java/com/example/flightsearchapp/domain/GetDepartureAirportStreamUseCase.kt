b75e93e34ed3a440e81cf444892576d29b348beb
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-28 23:18:26 +0900

    Create GetDepartureAirportStreamUseCase.

---
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/domain/GetDepartureAirportStreamUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.flightsearchapp.domain
+
+import com.example.flightsearchapp.data.AirportsRepository
+import com.example.flightsearchapp.data.database.Airport
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetDepartureAirportStreamUseCase @Inject constructor(
+    private val airportsRepository: AirportsRepository,
+) {
+    operator fun invoke(departureId: Long): Flow<Airport> {
+        return airportsRepository.getDepartureAirportStream(departureId)
+    }
+}
