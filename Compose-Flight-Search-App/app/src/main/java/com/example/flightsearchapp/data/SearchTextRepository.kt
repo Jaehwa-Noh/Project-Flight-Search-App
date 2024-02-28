dc88346b2b964cf7139241fb8a32614b693edc48
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-28 23:21:09 +0900

    Add SingleTon.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/SearchTextRepository.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/SearchTextRepository.kt
@@ -10,7 +10,9 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class SearchTextRepository @Inject constructor(
     private val dataStore: DataStore<Preferences>
 ) {
