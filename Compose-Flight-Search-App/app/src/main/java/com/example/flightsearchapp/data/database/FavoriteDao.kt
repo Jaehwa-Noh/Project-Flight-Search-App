729c285e61a601fd8bac6c16d07917e6dc94eb7b
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-29 21:41:26 +0900

    Add getAllFavoritesStreams, insert and delete.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/database/FavoriteDao.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/database/FavoriteDao.kt
@@ -1,7 +1,29 @@
 package com.example.flightsearchapp.data.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavoriteDao {
+    @Query(
+        """
+            SELECT *
+              FROM favorite
+        """
+    )
+    fun getAllFavoritesStream(): Flow<List<Favorite>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(favorite: Favorite)
+
+    @Query(
+        """
+            DELETE FROM favorite
+             WHERE id = :favoriteId
+        """
+    )
+    fun delete(favoriteId: Long)
 }
