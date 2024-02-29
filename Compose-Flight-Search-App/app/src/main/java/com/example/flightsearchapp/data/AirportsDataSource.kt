6d9564342f8caafa6ae68cb54ba9f9f2479ac0d8
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-29 20:43:34 +0900

    Remove suspend and IO Dispatchers

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/AirportsDataSource.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/AirportsDataSource.kt
@@ -2,15 +2,12 @@ package com.example.flightsearchapp.data
 
 import com.example.flightsearchapp.data.database.Airport
 import com.example.flightsearchapp.data.database.AirportDao
-import com.example.flightsearchapp.di.DispatcherIO
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface AirportsDataSource {
-    suspend fun getSuggestionsStream(query: String): Flow<List<Airport>>
+    fun getSuggestionsStream(query: String): Flow<List<Airport>>
     fun getDepartureAirportStream(departureId: Long): Flow<Airport>
     fun getArriveAirportsStream(departureId: Long): Flow<List<Airport>>
 }
@@ -18,12 +15,9 @@ interface AirportsDataSource {
 @Singleton
 class LocalAirportsDataSource @Inject constructor(
     private val airportDao: AirportDao,
-    @DispatcherIO private val ioDispatcher: CoroutineDispatcher
 ) : AirportsDataSource {
-    override suspend fun getSuggestionsStream(query: String): Flow<List<Airport>> {
-        return withContext(ioDispatcher) {
-            airportDao.getSuggestionsStream(query = query)
-        }
+    override fun getSuggestionsStream(query: String): Flow<List<Airport>> {
+        return airportDao.getSuggestionsStream(query = query)
     }
 
     override fun getDepartureAirportStream(departureId: Long): Flow<Airport> {
