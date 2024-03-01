c710251148c419afb9a27532200243044d4cd01a
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-01 12:13:31 +0900

    Add showFlightUiState parameter, LazyColumn and Preview.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/FlightsListScreen.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/FlightsListScreen.kt
@@ -1,17 +1,58 @@
 package com.example.flightsearchapp.ui
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.flightsearchapp.ui.model.FlightModel
+import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
 
 @Composable
 fun FlightsListScreen(
     modifier: Modifier = Modifier,
-    searchScreenUiState: SearchScreenUiState
+    showFlightUiState: ShowFlightUiState
 ) {
-    if (searchScreenUiState is SearchScreenUiState.SuccessSuggests) {
+    if (showFlightUiState is ShowFlightUiState.SelectSuggest) {
+        LazyColumn(modifier = modifier) {
+            items(
+                showFlightUiState.allFlights,
+                key = {
+                    it.id
+                },
+            ) { flight ->
+                FlightItemCard(flight = flight)
+            }
+        }
+    }
+}
 
-        Text("List ${searchScreenUiState.result}", modifier = modifier)
 
+@Preview(showBackground = true)
+@Composable
+fun FlightsListScreenPreview() {
+    FlightSearchAppTheme(dynamicColor = false) {
+        FlightsListScreen(
+            showFlightUiState = ShowFlightUiState.SelectSuggest(
+                allFlights = listOf(
+                    FlightModel(
+                        id = "1",
+                        departureIataCode = "ABC",
+                        departureName = "Departure Airport",
+                        arriveIataCode = "EFG",
+                        arriveName = "Arrive Airport",
+                        isBookmarked = false,
+                    ),
+                    FlightModel(
+                        id = "2",
+                        departureIataCode = "DEF",
+                        departureName = "Departure Airport",
+                        arriveIataCode = "HIJ",
+                        arriveName = "Arrive Airport",
+                        isBookmarked = true,
+                    ),
+                ),
+            )
+        )
     }
 }
