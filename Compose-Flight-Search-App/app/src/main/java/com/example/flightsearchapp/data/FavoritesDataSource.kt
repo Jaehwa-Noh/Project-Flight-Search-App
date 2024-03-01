81d2e3531f18dba1fbb255dbda2daef45bbf48f8
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-01 19:03:23 +0900

    Add suspend at insertFavorite and deleteFavorite, and add ioDispatcher.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/FavoritesDataSource.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/FavoritesDataSource.kt
@@ -2,29 +2,40 @@ package com.example.flightsearchapp.data
 
 import com.example.flightsearchapp.data.database.Favorite
 import com.example.flightsearchapp.data.database.FavoriteDao
+import com.example.flightsearchapp.di.DispatcherIO
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface FavoritesDataSource {
     fun getFavoritesStream(): Flow<List<Favorite>>
-    fun insertFavorite(favorite: Favorite)
-    fun deleteFavorite(favoriteId: Long)
+    suspend fun insertFavorite(favorite: Favorite)
+    suspend fun deleteFavorite(departureCode: String, arriveCode: String)
 }
 
 @Singleton
 class LocalFavoritesDataSource @Inject constructor(
     private val favoriteDao: FavoriteDao,
+    @DispatcherIO private val ioDispatcher: CoroutineDispatcher,
 ) : FavoritesDataSource {
     override fun getFavoritesStream(): Flow<List<Favorite>> {
         return favoriteDao.getAllFavoritesStream()
     }
 
-    override fun insertFavorite(favorite: Favorite) {
-        favoriteDao.insert(favorite = favorite)
+    override suspend fun insertFavorite(favorite: Favorite) {
+        withContext(ioDispatcher) {
+            favoriteDao.insert(favorite = favorite)
+        }
     }
 
-    override fun deleteFavorite(favoriteId: Long) {
-        favoriteDao.delete(favoriteId = favoriteId)
+    override suspend fun deleteFavorite(departureCode: String, arriveCode: String) {
+        withContext(ioDispatcher) {
+            favoriteDao.delete(
+                departureCode = departureCode,
+                arriveCode = arriveCode,
+            )
+        }
     }
 }
