8be90a9ccc5152c64b4d432caa6793f582b210ae
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-01 12:12:33 +0900

    Add Informative Text.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/SearchScreen.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/SearchScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -80,7 +82,10 @@ fun SearchScreen(modifier: Modifier = Modifier) {
                     }
 
                     is SearchScreenUiState.ShowFavorite -> {
-
+                        Text(
+                            "Favorite routes",
+                            modifier = Modifier.padding(vertical = 8.dp)
+                        )
                     }
 
                     else -> Unit
@@ -88,6 +93,15 @@ fun SearchScreen(modifier: Modifier = Modifier) {
             }
 
             is ShowFlightUiState.SelectSuggest -> {
+                Text(
+                    "Flights from ${
+                        (showFlightUiState.value as ShowFlightUiState.SelectSuggest)
+                            .allFlights
+                            .first()
+                            .departureIataCode
+                    }",
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
                 FlightsListScreen(
                     modifier = Modifier.fillMaxSize(),
                     showFlightUiState = showFlightUiState.value,
