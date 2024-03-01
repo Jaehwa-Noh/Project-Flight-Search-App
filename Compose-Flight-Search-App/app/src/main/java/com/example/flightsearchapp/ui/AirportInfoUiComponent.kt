9997c7a88582600df7f909bb83cfb84fc62e1bf8
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-01 12:08:01 +0900

    Apply bold fontWeight.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/AirportInfoUiComponent.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/AirportInfoUiComponent.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -24,7 +24,7 @@ fun AirportInfoUiComponent(
     ) {
         Text(
             text = iataCode,
-            style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.Bold,
         )
         Spacer(modifier = Modifier.width(8.dp))
         Text(name)
