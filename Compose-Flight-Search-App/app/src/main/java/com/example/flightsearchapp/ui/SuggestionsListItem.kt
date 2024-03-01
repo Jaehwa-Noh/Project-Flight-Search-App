8206751d3d3e2826304966089ec9f09727980f80
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-01 10:30:20 +0900

    Apply AirportInfoUiComponent.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/SuggestionsListItem.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/ui/SuggestionsListItem.kt
@@ -1,17 +1,9 @@
 package com.example.flightsearchapp.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.flightsearchapp.ui.model.SuggestionAirportModel
 import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
 
@@ -22,22 +14,13 @@ fun SuggestionsListItem(
     onItemClick: (Long) -> Unit
 ) {
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    AirportInfoUiComponent(iataCode = suggestionAirportModel.iataCode,
+        name = suggestionAirportModel.name,
         modifier = modifier
             .clickable {
                 onItemClick(suggestionAirportModel.id)
             }
-            .padding(vertical = 8.dp),
-    ) {
-        Text(
-            text = suggestionAirportModel.iataCode,
-            style = MaterialTheme.typography.titleSmall
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(text = suggestionAirportModel.name)
-    }
-
+    )
 }
 
 
