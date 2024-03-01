8e20d1cffe7ecdc063015b05eccabed94ae8d6b1
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-01 09:44:58 +0900

    Apply combine, and add getDepartureAirportStreamUseCase.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/domain/GetAllFlightsStreamUseCase.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/domain/GetAllFlightsStreamUseCase.kt
@@ -1,35 +1,62 @@
 package com.example.flightsearchapp.domain
 
+import com.example.flightsearchapp.di.DispatcherDefault
 import com.example.flightsearchapp.ui.model.FlightModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetAllFlightsStreamUseCase @Inject constructor(
     private val getArriveAirportsStreamUseCase: GetArriveAirportsStreamUseCase,
     private val getDepartureAirportStreamUseCase: GetDepartureAirportStreamUseCase,
+    private val getAllFavoritesStreamUseCase: GetAllFavoritesStreamUseCase,
+    @DispatcherDefault private val defaultDispatcher: CoroutineDispatcher,
 ) {
+    @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(departureId: Long): Flow<List<FlightModel>> {
-        return getDepartureAirportStreamUseCase(departureId = departureId)
-            .combine(
-                getArriveAirportsStreamUseCase(departureId = departureId),
-            ) { departureAirport, arriveAirports ->
-                val flightsModel = mutableListOf<FlightModel>()
+        return getDepartureAirportStreamUseCase(departureId = departureId).flatMapLatest { departureAirport ->
+            if (departureAirport == null) {
+                flowOf(emptyList())
+            } else {
+                combine(
+                    getArriveAirportsStreamUseCase(departureId = departureId),
+                    getAllFavoritesStreamUseCase(),
+                ) { arriveAirports, favorites ->
+                    withContext(defaultDispatcher) {
+                        val flightsModel = mutableListOf<FlightModel>()
+                        val departureCodes = mutableListOf<String>()
+                        val arriveCodes = mutableListOf<String>()
 
-                arriveAirports.forEach { arriveAirport ->
-                    flightsModel.add(
-                        FlightModel(
-                            id = "${departureAirport.id}_${arriveAirport.id}",
-                            departureId = departureAirport.id,
-                            departureIataCode = departureAirport.iataCode,
-                            departureName = departureAirport.name,
-                            arriveId = arriveAirport.id,
-                            arriveName = arriveAirport.name,
-                            arriveIataCode = arriveAirport.iataCode,
-                        )
-                    )
+                        favorites.forEach { favorite ->
+                            departureCodes.add(favorite.departureCode)
+                            arriveCodes.add(favorite.destinationCode)
+                        }
+
+                        arriveAirports.forEach { arriveAirport ->
+                            flightsModel.add(
+                                FlightModel(
+                                    id = "${departureAirport.id}_${arriveAirport.id}",
+                                    departureIataCode = departureAirport.iataCode,
+                                    departureName = departureAirport.name,
+                                    arriveName = arriveAirport.name,
+                                    arriveIataCode = arriveAirport.iataCode,
+                                    isBookmarked =
+                                    departureCodes
+                                        .contains(departureAirport.iataCode) &&
+                                            arriveCodes
+                                                .contains(arriveAirport.iataCode),
+                                )
+                            )
+                        }
+                        return@withContext flightsModel
+                    }
                 }
-                return@combine flightsModel
             }
+        }
     }
 }
