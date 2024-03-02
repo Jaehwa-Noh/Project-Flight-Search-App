c46f08efd23b5a04530a6833e44760ac80002f32
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-02 12:48:40 +0900

    Change name and ioDispatcher, and create getAirportByCode function - getAirportsByCodeOrNameStream

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/AirportsDataSource.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/data/AirportsDataSource.kt
@@ -2,22 +2,27 @@ package com.example.flightsearchapp.data
 
 import com.example.flightsearchapp.data.database.Airport
 import com.example.flightsearchapp.data.database.AirportDao
+import com.example.flightsearchapp.di.DispatcherIO
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface AirportsDataSource {
-    fun getSuggestionsStream(query: String): Flow<List<Airport>>
+    fun getAirportsByCodeOrNameStream(query: String): Flow<List<Airport>>
     fun getAirportNullByIdStream(airportId: Long): Flow<Airport?>
     fun getAirportsStream(airportId: Long): Flow<List<Airport>>
+    suspend fun getAirportByCode(airportCode: String): Airport
 }
 
 @Singleton
 class LocalAirportsDataSource @Inject constructor(
     private val airportDao: AirportDao,
+    @DispatcherIO private val ioDispatcher: CoroutineDispatcher,
 ) : AirportsDataSource {
-    override fun getSuggestionsStream(query: String): Flow<List<Airport>> {
-        return airportDao.getSuggestionsStream(query = query)
+    override fun getAirportsByCodeOrNameStream(query: String): Flow<List<Airport>> {
+        return airportDao.getAirportsByCodeOrNameStream(query = query)
     }
 
     override fun getAirportNullByIdStream(airportId: Long): Flow<Airport?> {
@@ -26,5 +31,9 @@ class LocalAirportsDataSource @Inject constructor(
 
     override fun getAirportsStream(airportId: Long): Flow<List<Airport>> {
         return airportDao.getAirportsStream(airportId = airportId)
+    }
+
+    override suspend fun getAirportByCode(airportCode: String): Airport {
+        return withContext(ioDispatcher) { airportDao.getAirportByCode(airportCode = airportCode) }
     }
 }
