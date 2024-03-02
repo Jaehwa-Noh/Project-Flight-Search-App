7531e766204bd043d8315a34a00417a848562766
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-02 12:52:46 +0900

    Transfer function to AllFlightsRepository.

--- Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/domain/GetAllFlightsStreamUseCase.kt
+++ Compose-Flight-Search-App/app/src/main/java/com/example/flightsearchapp/domain/GetAllFlightsStreamUseCase.kt
@@ -1,62 +1,14 @@
 package com.example.flightsearchapp.domain
 
-import com.example.flightsearchapp.di.DispatcherDefault
+import com.example.flightsearchapp.data.AllFlightsRepository
 import com.example.flightsearchapp.ui.model.FlightModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetAllFlightsStreamUseCase @Inject constructor(
-    private val getAirportsStreamUseCase: GetAirportsStreamUseCase,
-    private val getAirportNullByIdStreamUseCase: GetAirportNullByIdStreamUseCase,
-    private val getAllFavoritesStreamUseCase: GetAllFavoritesStreamUseCase,
-    @DispatcherDefault private val defaultDispatcher: CoroutineDispatcher,
+    private val allFlightsRepository: AllFlightsRepository,
 ) {
-    @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(departureId: Long): Flow<List<FlightModel>> {
-        return getAirportNullByIdStreamUseCase(airportId = departureId).flatMapLatest { departureAirport ->
-            if (departureAirport == null) {
-                flowOf(emptyList())
-            } else {
-                combine(
-                    getAirportsStreamUseCase(airportId = departureId),
-                    getAllFavoritesStreamUseCase(),
-                ) { arriveAirports, favorites ->
-                    withContext(defaultDispatcher) {
-                        val flightsModel = mutableListOf<FlightModel>()
-                        val departureCodes = mutableListOf<String>()
-                        val arriveCodes = mutableListOf<String>()
-
-                        favorites.forEach { favorite ->
-                            departureCodes.add(favorite.departureCode)
-                            arriveCodes.add(favorite.destinationCode)
-                        }
-
-                        arriveAirports.forEach { arriveAirport ->
-                            flightsModel.add(
-                                FlightModel(
-                                    id = "${departureAirport.id}_${arriveAirport.id}",
-                                    departureIataCode = departureAirport.iataCode,
-                                    departureName = departureAirport.name,
-                                    arriveName = arriveAirport.name,
-                                    arriveIataCode = arriveAirport.iataCode,
-                                    isBookmarked =
-                                    departureCodes
-                                        .contains(departureAirport.iataCode) &&
-                                            arriveCodes
-                                                .contains(arriveAirport.iataCode),
-                                )
-                            )
-                        }
-                        return@withContext flightsModel
-                    }
-                }
-            }
-        }
+        return allFlightsRepository.getAllFlightsStream(departureId = departureId)
     }
 }
