b10025bff5d8f62c4d957136fe03c6b04705d393
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-07 22:25:57 +0900

    Add AirportFtsEntity and AirportFtsDao.

--- Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/data/database/AppDatabase.kt
+++ Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/data/database/AppDatabase.kt
@@ -5,10 +5,15 @@ import androidx.room.RoomDatabase
 
 
 @Database(
-    entities = [AirportEntity::class, FavoriteEntity::class],
+    entities = [
+        AirportEntity::class,
+        FavoriteEntity::class,
+        AirportFtsEntity::class,
+    ],
     version = 1,
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun airportDao(): AirportDao
     abstract fun favoriteDao(): FavoriteDao
+    abstract fun airportFtsDao(): AirportFtsDao
 }
