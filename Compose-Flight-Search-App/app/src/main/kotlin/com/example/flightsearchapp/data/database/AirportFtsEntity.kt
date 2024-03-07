270d5df3229ca070d0161b79a9881a972d208b41
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-07 16:47:44 +0900

    Create AirportFtsEntity

---
+++ Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/data/database/AirportFtsEntity.kt
@@ -0,0 +1,18 @@
+package com.example.flightsearchapp.data.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Fts4
+
+@Entity(tableName = "airportFts")
+@Fts4
+data class AirportFtsEntity(
+    @ColumnInfo
+    val airportId: String,
+
+    @ColumnInfo(name = "iata_code")
+    val iataCode: String,
+
+    @ColumnInfo
+    val name: String,
+)
