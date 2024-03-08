34cf3f71eba3bed1c6d5557c8d9412f32c2ec934
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-08 20:00:55 +0900

    Move functions to RepositoryModule.

--- Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/di/RepositoryModule.kt
+++ Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/di/RepositoryModule.kt
@@ -1,19 +1,19 @@
 package com.example.flightsearchapp.di
 
+import com.example.flightsearchapp.data.AirportsRepository
 import com.example.flightsearchapp.data.FavoritesRepository
+import com.example.flightsearchapp.data.InDiskAirportsRepository
 import com.example.flightsearchapp.data.InDiskFavoritesRepository
 import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-abstract class FavoritesRepositoryModule {
+abstract class RepositoryModule @Inject constructor() {
+    @Singleton
+    @Binds
+    abstract fun bindAirportRepository(impl: InDiskAirportsRepository): AirportsRepository
 
     @Singleton
     @Binds
     abstract fun bindFavoritesRepository(impl: InDiskFavoritesRepository): FavoritesRepository
-
 }
