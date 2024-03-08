027f6881dc24e5e7c1e6a80b4ee3c040460cdc4f
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-08 20:26:12 +0900

    Create CoroutineScopesModule.

---
+++ Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/di/CoroutineScopesModule.kt
@@ -0,0 +1,21 @@
+package com.example.flightsearchapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineScopesModule {
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun provideCoroutineScope(
+        @DispatcherDefault defaultDispatcher: CoroutineDispatcher
+    ): CoroutineScope = CoroutineScope(SupervisorJob() + defaultDispatcher)
+}
