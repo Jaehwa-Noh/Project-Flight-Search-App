96bc88bc23cb91c9659d1b94c09c0c8e7836c9ee
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-08 21:42:23 +0900

    Transfer airportsEntities to airportsFtsEntities.

--- Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/SearchFlightApplication.kt
+++ Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/SearchFlightApplication.kt
@@ -1,7 +1,37 @@
 package com.example.flightsearchapp
 
 import android.app.Application
+import com.example.flightsearchapp.data.AirportsFtsRepository
+import com.example.flightsearchapp.data.AirportsRepository
+import com.example.flightsearchapp.data.database.asFtsEntity
+import com.example.flightsearchapp.di.ApplicationScope
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
-class SearchFlightApplication: Application()
+class SearchFlightApplication : Application() {
+    @Inject
+    lateinit var airportsRepository: AirportsRepository
+
+    @Inject
+    lateinit var airportsFtsRepository: AirportsFtsRepository
+
+    @Inject
+    @ApplicationScope
+    lateinit var applicationScope: CoroutineScope
+
+    override fun onCreate() {
+        super.onCreate()
+        applicationScope.launch {
+            airportsFtsRepository.upsertAirports(
+                airportsRepository.getAllAirportsEntities().map { airportEntity ->
+                    airportEntity.asFtsEntity()
+                }
+            )
+        }
+    }
+}
+
+
