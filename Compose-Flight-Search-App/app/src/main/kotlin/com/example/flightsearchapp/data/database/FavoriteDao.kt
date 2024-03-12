1537a029bb6fb5657f1b0952ae78b1d90803847f
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-12 21:29:40 +0900

    Create getFavoriteWithAirports function.

--- Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/data/database/FavoriteDao.kt
+++ Compose-Flight-Search-App/app/src/main/kotlin/com/example/flightsearchapp/data/database/FavoriteDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -26,4 +27,13 @@ interface FavoriteDao {
         """
     )
     fun delete(departureCode: String, arriveCode: String)
+
+    @Transaction
+    @Query(
+        """
+            SELECT *
+              FROM favorite
+        """
+    )
+    fun getFavoriteWithAirports(): Flow<List<FavoriteWithAirports>>
 }
