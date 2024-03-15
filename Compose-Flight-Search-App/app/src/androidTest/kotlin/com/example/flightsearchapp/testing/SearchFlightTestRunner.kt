2a5677027280ee873a9b1845193b5e97cac87705
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-15 22:09:38 +0900

    Create SearchFlightTestRunner.

---
+++ Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/SearchFlightTestRunner.kt
@@ -0,0 +1,17 @@
+package com.example.flightsearchapp.testing
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+
+class SearchFlightTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.getName(), context)
+    }
+}
