99f2988caedfd32856a33582ef98e997044eaa09
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-15 20:38:33 +0900

    Update gradle - hilt-android:2.51 - hilt-android-compiler:2.51 - compose-bom:2024.02.02
    androidTest
    - hilt-android-testing:2.51
    - hilt-compiler:2.51
    - compose-bom:2024.02.02

--- Compose-Flight-Search-App/app/build.gradle.kts
+++ Compose-Flight-Search-App/app/build.gradle.kts
@@ -72,13 +72,17 @@ dependencies {
     implementation("androidx.room:room-ktx:$roomVersion")
 
     // hilt
-    implementation("com.google.dagger:hilt-android:2.50")
-    ksp("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("com.google.dagger:hilt-android:2.51")
+    ksp("com.google.dagger:hilt-android-compiler:2.51")
+
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.51")
+    androidTestAnnotationProcessor("com.google.dagger:hilt-compiler:2.51")
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -86,7 +90,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
