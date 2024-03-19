e627c871feddf42a6ffd5e0454148092d60a133a
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-19 21:34:27 +0900

    Add `@Inject`.

--- Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/repository/TestSearchTextRepository.kt
+++ Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/repository/TestSearchTextRepository.kt
@@ -3,8 +3,9 @@ package com.example.flightsearchapp.testing.repository
 import com.example.flightsearchapp.data.SearchTextRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
 
-class TestSearchTextRepository: SearchTextRepository {
+class TestSearchTextRepository @Inject constructor(): SearchTextRepository {
     override val savedSearchText: Flow<String> = flowOf()
     override suspend fun saveSearchText(searchText: String) = Unit
 }
