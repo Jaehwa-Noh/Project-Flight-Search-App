21395f26568b436443e594c67e5ab640756a65b4
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-20 09:06:45 +0900

    Move to extension.

--- Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/database/AirportEntitiesTestData.kt
+++ Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/database/AirportEntitiesTestData.kt
@@ -1,17 +1,11 @@
 package com.example.flightsearchapp.testing.database
 
 import com.example.flightsearchapp.data.database.AirportEntity
+import com.example.flightsearchapp.testing.database.entityextension.testAirportEntity
 
 val airportEntitiesTestData: List<AirportEntity> = listOf(
-    testFavoriteEntity(1, "ABC"),
-    testFavoriteEntity(2, "CDE"),
-    testFavoriteEntity(3, "EFG"),
-    testFavoriteEntity(4, "GHI"),
-)
-
-private fun testFavoriteEntity(id: Long, code: String) = AirportEntity(
-    id = id,
-    name = "name$id",
-    iataCode = code,
-    passengers = id,
+    testAirportEntity(1, "ABC"),
+    testAirportEntity(2, "CDE"),
+    testAirportEntity(3, "EFG"),
+    testAirportEntity(4, "GHI"),
 )
