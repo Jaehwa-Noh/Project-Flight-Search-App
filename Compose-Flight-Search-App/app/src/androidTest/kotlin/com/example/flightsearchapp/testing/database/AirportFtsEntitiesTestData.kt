f0e3ef83a9f525ee90ac76b04b750f70ed7d66ce
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-20 09:06:19 +0900

    Create airportFtsEntitiesTestData.

---
+++ Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/database/AirportFtsEntitiesTestData.kt
@@ -0,0 +1,10 @@
+package com.example.flightsearchapp.testing.database
+
+import com.example.flightsearchapp.data.database.AirportFtsEntity
+import com.example.flightsearchapp.data.database.asFtsEntity
+import com.example.flightsearchapp.testing.database.entityextension.testAirportEntity
+
+val airportFtsEntitiesTestData: List<AirportFtsEntity> = listOf(
+    testAirportEntity(1, "HAM").asFtsEntity(),
+    testAirportEntity(2, "CDE").asFtsEntity(),
+)
