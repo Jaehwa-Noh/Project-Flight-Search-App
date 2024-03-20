02904597ea3ad3b8abb2b861916ea645886c1ab7
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-20 09:11:40 +0900

    Create TextSearchTextRepository.

--- Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/repository/TestSearchTextRepository.kt
+++ Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/repository/TestSearchTextRepository.kt
@@ -2,10 +2,17 @@ package com.example.flightsearchapp.testing.repository
 
 import com.example.flightsearchapp.data.SearchTextRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 class TestSearchTextRepository @Inject constructor(): SearchTextRepository {
-    override val savedSearchText: Flow<String> = flowOf()
-    override suspend fun saveSearchText(searchText: String) = Unit
+
+    private val _mySearchText = MutableStateFlow("")
+
+    override val savedSearchText: Flow<String> = _mySearchText.asStateFlow()
+    override suspend fun saveSearchText(searchText: String) {
+        _mySearchText.update { searchText }
+    }
 }
