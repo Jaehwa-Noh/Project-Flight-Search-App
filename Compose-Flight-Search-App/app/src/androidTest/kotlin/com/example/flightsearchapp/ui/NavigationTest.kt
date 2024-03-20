45d1803620a1b9726fde02afc204e911e473686d
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-20 09:12:40 +0900

    Set setup.

--- Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/ui/NavigationTest.kt
+++ Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/ui/NavigationTest.kt
@@ -1,20 +1,27 @@
 package com.example.flightsearchapp.ui
 
-import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
+import androidx.test.espresso.Espresso
 import com.example.flightsearchapp.MainActivity
+import com.example.flightsearchapp.data.AirportsFtsRepository
+import com.example.flightsearchapp.testing.database.airportFtsEntitiesTestData
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
 
 @HiltAndroidTest
 class NavigationTest {
@@ -24,8 +31,18 @@ class NavigationTest {
     @get:Rule(order = 1)
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
+    @Inject
+    lateinit var airportsFtsRepository: AirportsFtsRepository
+
     @Before
-    fun setUp() = hiltRule.inject()
+    fun setUp() {
+        hiltRule.inject()
+        runTest {
+            airportsFtsRepository.deleteAndInsertAll(
+                airportFtsEntitiesTestData
+            )
+        }
+    }
 
     @Test
     fun whenOpenApp_FirstScreenIsSearchScreen() {
