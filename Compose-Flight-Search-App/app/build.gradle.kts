2991c71e6f857c8e9ec4f46ff0b0c82f698ee363
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-21 18:10:51 +0900

    Sort alphabetically.

--- Compose-Flight-Search-App/app/build.gradle.kts
+++ Compose-Flight-Search-App/app/build.gradle.kts
@@ -52,49 +52,41 @@ android {
 }
 
 dependencies {
-
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1-Beta")
-
-    // Kotlin test
-    testImplementation(kotlin("test"))
-
-     // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-
-    // lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
-
-    // room database
     val roomVersion = "2.6.1"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
 
-    // hilt
-    implementation("com.google.dagger:hilt-android:2.51")
+    ksp("androidx.room:room-compiler:$roomVersion")
     ksp("com.google.dagger:hilt-android-compiler:2.51")
 
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.51")
     kspAndroidTest("com.google.dagger:hilt-compiler:2.51")
 
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("com.google.dagger:hilt-android:2.51")
+    implementation(kotlin("test"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
+
     testImplementation("junit:junit:4.13.2")
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1-Beta")
+
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.51")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation(kotlin("test"))
+
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
 }
