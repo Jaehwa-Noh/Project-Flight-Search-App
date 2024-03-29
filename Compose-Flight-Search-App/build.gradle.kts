ec0344ff4ac8dc36766662b54dcb80462cdc8eed
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-29 23:45:42 +0900

    Gradle update - id("com.diffplug.spotless") version "6.25.0"

--- Compose-Flight-Search-App/build.gradle.kts
+++ Compose-Flight-Search-App/build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.51" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.diffplug.spotless") version "6.25.0"
 }
