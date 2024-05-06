148f9e31b51d6169594fe8cbf351c252e2e488aa
Jaehwa Noh <shwoghk14@gmail.com>
2024-05-06 20:45:47 +0900

    Create AirportsFakeDataSource for AirportsDataSource

---
+++ Compose-Flight-Search-App/app/src/test/kotlin/com/example/flightsearchapp/fake/AirportsFakeDataSource.kt
@@ -0,0 +1,70 @@
+package com.example.flightsearchapp.fake
+
+import com.example.flightsearchapp.data.AirportsDataSource
+import com.example.flightsearchapp.data.database.AirportEntity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
+
+/**
+ * FakeDataSource for [AirportsDataSource].
+ */
+class AirportsFakeDataSource : AirportsDataSource {
+    private val _airportEntityFlow: MutableSharedFlow<List<AirportEntity>> = MutableSharedFlow(
+        replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST,
+    )
+    val airportEntityFlow: Flow<List<AirportEntity>> = _airportEntityFlow.asSharedFlow()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun getAirportsByCodeOrNameStream(query: String): Flow<List<AirportEntity>> =
+        airportEntityFlow.mapLatest { airportsEntities ->
+            airportsEntities.filter { airportEntity ->
+                airportEntity.name == query || airportEntity.iataCode == query
+            }
+        }
+
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun getAirportNullByIdStream(airportId: Long): Flow<AirportEntity?> =
+        airportEntityFlow.mapLatest { airportsEntities ->
+
+            val filteredAirport = airportsEntities.filter { airportEntity ->
+                airportEntity.id == airportId
+            }
+            if (filteredAirport.isEmpty()) {
+                return@mapLatest null
+            }
+
+            filteredAirport.first()
+        }
+
+
+    override fun getAirportsStream(airportId: Long): Flow<List<AirportEntity>> =
+        airportEntityFlow.map { airportsEntities ->
+            airportsEntities.filter { airportEntity ->
+                airportEntity.id == airportId
+            }
+        }
+
+
+    override suspend fun getAirportByCode(airportCode: String): AirportEntity =
+        airportEntityFlow.map { airportsEntities ->
+            val filteredAirportsEntities = airportsEntities.filter { airportEntity ->
+                airportEntity.iataCode == airportCode
+            }
+            filteredAirportsEntities
+        }.first().first()
+
+
+    override suspend fun getAllAirportsEntities(): List<AirportEntity> =
+        airportEntityFlow.first()
+
+    fun insertAirportsEntities(airportsEntities: List<AirportEntity>) {
+        _airportEntityFlow.tryEmit(airportsEntities)
+    }
+}
