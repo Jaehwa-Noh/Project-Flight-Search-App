487eac40dda490c19ae71b0ff8731cbf1affad12
Jaehwa Noh <shwoghk14@gmail.com>
2024-05-07 22:36:36 +0900

    Create AirportsRepositoryTest - searchQuery_search_matchSuggestion - searchQuery_search_matchAirports - airportId_search_matchAirport - airportId_search_matchAirports - airportCode_search_matchAirport - airport_getAll_matchAirports

---
+++ Compose-Flight-Search-App/app/src/test/kotlin/com/example/flightsearchapp/AirportsRepositoryTest.kt
@@ -0,0 +1,80 @@
+package com.example.flightsearchapp
+
+import com.example.flightsearchapp.data.AirportsRepository
+import com.example.flightsearchapp.data.InDiskAirportsRepository
+import com.example.flightsearchapp.fake.AirportsFakeDataSource
+import com.example.flightsearchapp.testing.model.database.airportEntitiesTestData
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+/**
+ * Instrumented test for [AirportsRepository]
+ */
+class AirportsRepositoryTest {
+
+    private lateinit var airportsRepository: AirportsRepository
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val testDispatcher = UnconfinedTestDispatcher()
+
+    @BeforeTest
+    fun setUp() {
+        val airportsDataSource = AirportsFakeDataSource()
+
+        airportsDataSource.insertAirportsEntities(
+            airportEntitiesTestData,
+        )
+
+        airportsRepository = InDiskAirportsRepository(
+            airportsDataSource = airportsDataSource,
+            defaultDispatcher = testDispatcher,
+        )
+    }
+
+    @Test
+    fun searchQuery_search_matchSuggestion() = runTest {
+        val result = airportsRepository.getSuggestionsStream("ABC").first().first()
+
+        assertEquals(result.iataCode, "ABC")
+    }
+
+    @Test
+    fun searchQuery_search_matchAirports() = runTest {
+        val result = airportsRepository.getAirportsByCodeOrNameStream("ABC").first().first()
+
+        assertEquals(result.iataCode, "ABC")
+    }
+
+    @Test
+    fun airportId_search_matchAirport() = runTest {
+        val result = airportsRepository.getAirportNullByIdStream(1).first()
+
+        assertEquals(result?.iataCode, "ABC")
+    }
+
+    @Test
+    fun airportId_search_matchAirports() = runTest {
+        val result = airportsRepository.getAirportsStream(1).first().first()
+
+        assertEquals(result.iataCode, "ABC")
+    }
+
+    @Test
+    fun airportCode_search_matchAirport() = runTest {
+        val result = airportsRepository.getAirportByCode("CDE")
+
+        assertEquals(result.iataCode, "CDE")
+    }
+
+    @Test
+    fun airport_getAll_matchAirports() = runTest {
+        val result = airportsRepository.getAllAirportsEntities()
+        assertTrue { result.size == airportEntitiesTestData.size }
+    }
+}
