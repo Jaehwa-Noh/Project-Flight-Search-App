bef8382c855e2e41c7446fb9cdbf775bbdb0b812
Jaehwa Noh <shwoghk14@gmail.com>
2024-05-11 22:34:45 +0900

    Create FavoritesFakeDataSourceTest - favorites_getFavoriteWithAirports_matchResult - null_getFavoriteWithAirports_null - favorites_getFavoriteWithAirports_matchCount

---
+++ Compose-Flight-Search-App/app/src/test/kotlin/com/example/flightsearchapp/fake/FavoritesFakeDataSourceTest.kt
@@ -0,0 +1,66 @@
+package com.example.flightsearchapp.fake
+
+import com.example.flightsearchapp.testing.model.database.airportEntitiesTestData
+import com.example.flightsearchapp.testing.model.database.favoriteEntitiesTestData
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class FavoritesFakeDataSourceTest {
+    private lateinit var favoritesFakeDataSource: FavoritesFakeDataSource
+
+    @BeforeTest
+    fun setUp() {
+        val airportsFakeDataSource = AirportsFakeDataSource()
+        airportsFakeDataSource.insertAirportsEntities(airportEntitiesTestData)
+
+        favoritesFakeDataSource = FavoritesFakeDataSource(
+            airportsFakeDataSource = airportsFakeDataSource,
+        )
+    }
+
+    @Test
+    fun favorites_getFavoriteWithAirports_matchResult() = runTest {
+        favoriteEntitiesTestData.forEach {
+            favoritesFakeDataSource.insertFavorite(it)
+        }
+
+        val favoriteWithAirports = favoritesFakeDataSource.getFavoriteWithAirports().first()
+        val result = favoriteWithAirports.filter {
+            it.arriveAirport.iataCode == "CDE"
+        }
+
+        assertEquals(1, result.count())
+    }
+
+    @Test
+    fun null_getFavoriteWithAirports_null() = runTest {
+        val favoriteWithAirports = withTimeoutOrNull(1000) {
+            favoritesFakeDataSource.getFavoriteWithAirports().first()
+        }
+
+        val result = favoriteWithAirports?.filter {
+            it.arriveAirport.iataCode == "CDE"
+        }
+
+        assertNull(result?.count())
+    }
+
+    @Test
+    fun favorites_getFavoriteWithAirports_matchCount() = runTest {
+        favoriteEntitiesTestData.forEach {
+            favoritesFakeDataSource.insertFavorite(it)
+        }
+
+        val favoriteWithAirports = favoritesFakeDataSource.getFavoriteWithAirports().first()
+        val result = favoriteWithAirports.filter {
+            it.arriveAirport.iataCode == "Ich"
+        }
+
+        assertEquals(0, result.count())
+    }
+}
