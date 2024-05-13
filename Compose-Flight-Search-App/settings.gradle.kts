bc5ba951cf6a84c06d194bbb0361b194c70937db
Jaehwa Noh <shwoghk14@gmail.com>
2024-05-13 22:14:58 +0900

    Gradle update - Add "org.gradle.toolchains.foojay-resolver-convention" 0.8.0

--- Compose-Flight-Search-App/settings.gradle.kts
+++ Compose-Flight-Search-App/settings.gradle.kts
@@ -13,5 +13,9 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+}
+
 rootProject.name = "Flight Search App"
 include(":app")
