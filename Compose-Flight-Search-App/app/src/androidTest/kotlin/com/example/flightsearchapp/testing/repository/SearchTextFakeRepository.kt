2c0b3091f6eb86ee67875f449aa3422c6bb87e54
Jaehwa Noh <shwoghk14@gmail.com>
2024-05-15 10:50:09 +0900

    Rename SearchTextFakeRepository.

--- Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/repository/SearchTextFakeRepository.kt
+++ Compose-Flight-Search-App/app/src/androidTest/kotlin/com/example/flightsearchapp/testing/repository/SearchTextFakeRepository.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class TestSearchTextRepository @Inject constructor() : SearchTextRepository {
+class SearchTextFakeRepository @Inject constructor() : SearchTextRepository {
 
     private val mySearchText = MutableStateFlow("")
 
