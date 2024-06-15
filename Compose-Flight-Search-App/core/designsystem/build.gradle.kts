62b293206843daacba418d2e0fb2734b11c1700d
Jaehwa Noh <shwoghk14@gmail.com>
2024-06-15 20:10:59 +0900

    Add implementation - platform(libs.androidx.compose.bom) - libs.androidx.compose.runtime - libs.androidx.core.ktx - libs.androidx.material3

--- Compose-Flight-Search-App/core/designsystem/build.gradle.kts
+++ Compose-Flight-Search-App/core/designsystem/build.gradle.kts
@@ -34,9 +34,11 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
