f2d0a9f989b49d9ba30bfb7e97b2fee93711287a
Jaehwa Noh <shwoghk14@gmail.com>
2024-06-17 19:36:04 +0900

    Create :core:database module.

--- Compose-Flight-Search-App/settings.gradle.kts
+++ Compose-Flight-Search-App/settings.gradle.kts
@@ -21,6 +21,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "Flight-Search-App"
 include(":app")
+include(":core:database")
 include(":core:designsystem")
 include(":core:ui")
 include(":feature:searchscreen")
