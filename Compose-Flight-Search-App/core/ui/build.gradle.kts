5e1136efa0f86d06737d21b9ede671af1db3e9c2
Jaehwa Noh <shwoghk14@gmail.com>
2024-06-19 13:29:55 +0900

    Set implementation :core:ui.

--- Compose-Flight-Search-App/core/ui/build.gradle.kts
+++ Compose-Flight-Search-App/core/ui/build.gradle.kts
@@ -19,14 +19,13 @@ android {
 }
 
 dependencies {
-    api(projects.core.model)
-
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
 
     implementation(projects.core.designsystem)
+    implementation(projects.core.ui)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
