626a4cb3fcf753c902d0bece464136cf7dff9317
Jaehwa Noh <shwoghk14@gmail.com>
2024-06-19 15:22:15 +0900

    Remove asFtsEntity, asSuggestionAirport function.

--- Compose-Flight-Search-App/core/database/src/main/kotlin/com/example/flightsearchapp/core/database/model/AirportEntity.kt
+++ Compose-Flight-Search-App/core/database/src/main/kotlin/com/example/flightsearchapp/core/database/model/AirportEntity.kt
@@ -3,7 +3,6 @@ package com.example.flightsearchapp.core.database.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.flightsearchapp.core.model.SuggestionAirport
 
 @Entity(tableName = "airport")
 data class AirportEntity(
@@ -19,16 +18,3 @@ data class AirportEntity(
     @ColumnInfo
     val passengers: Long,
 )
-
-fun AirportEntity.asFtsEntity() = AirportFtsEntity(
-    iataCode = iataCode,
-    name = name,
-)
-
-fun AirportEntity.asSuggestionAirport() =
-    SuggestionAirport(
-        id = id,
-        iataCode = iataCode,
-        name = name,
-        passengers = passengers,
-    )
