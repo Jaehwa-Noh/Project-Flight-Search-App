f7cd7e479cb2d7e4e0f01999e9deadc7cfc736f8
Jaehwa Noh <shwoghk14@gmail.com>
2024-06-19 16:17:15 +0900

    remove testOptions.

--- Compose-Flight-Search-App/app/build.gradle.kts
+++ Compose-Flight-Search-App/app/build.gradle.kts
@@ -63,12 +63,6 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-
-    testOptions {
-        unitTests {
-            isIncludeAndroidResources = true
-        }
-    }
 }
 
 dependencies {
